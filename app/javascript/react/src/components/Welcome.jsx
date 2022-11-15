2b3e92aafee1da88c0b816e99393eb6ca04952c6
Renan Martins <renanmartins989@gmail.com>
2022-11-15 15:40:26 +0000

    react setup done

---
+++ app/javascript/react/src/components/Welcome.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+// function oriented component(ES6)
+const Welcome = () => {
+  return(
+    <div className='container'>
+      <h1>Rails 7 with React!</h1>
+      <p className='subtitle'>from ./react/src/components/Welcome.jsx</p>
+    </div>
+  )
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Welcome />, document.getElementById('welcome'))
+})
+
+export default Welcome
